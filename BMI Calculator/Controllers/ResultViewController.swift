a2673583ce45b4f5b6636671fa1bdb9d03afc758
Huamin Ni <imsophieni@gmail.com>
2020-08-01 22:04:54 -0700

    finished basic bmi calculation function

---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by huaminni on 8/1/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController:UIViewController {
+    var bmiValue : String? //因为最初是没有计算bmivalue的
+    @IBOutlet weak var suggestionLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var heightBar: UISlider!
+    
+    @IBOutlet weak var weightBar: UISlider!
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    var bmiValue : String?
+    
+    
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        let heightValue = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(heightValue)m"
+        
+    }
+    
+    
+
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        let weightValue = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weightValue)Kg"
+    }
+    
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        let weight = weightBar.value
+        let height = heightBar.value
+        let bmi = weight / pow(height, 2)
+        bmiValue = String(format: "%0.1f", bmi)
+ //这是在用编程实现链接两个file的情况下：
+//        let secondVC = SecondViewController()
+//        secondVC.bmivalue = String(format: "%0.1f", bmi)
+//        self.present(secondVC, animated: true, completion: nil)
+        
+        self.performSegue(withIdentifier: "toGoResult", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == "toGoResult" {
+            let destinationVC = segue.destination as! ResultViewController  //使用as完成了downcasting，因为这个prepare function是被override的，一开始它被设立的时候是不知道哪个controller的，需要我们告诉它
+            destinationVC.bmiValue = bmiValue
+        }
+    }
+    
+}
+
